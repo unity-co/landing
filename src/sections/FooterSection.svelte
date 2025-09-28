c2f2f0b0450dc7b78d0eb59fa6f206c9da24c0b7
diablo <progzone122@gmail.com>
2025-09-28 14:55:31 +0300

    edit footer

--- src/sections/FooterSection.svelte
+++ src/sections/FooterSection.svelte
@@ -1,3 +1,12 @@
-<div class="mt-22 flex w-full justify-center bg-[#2f6a80] p-4">
-	<p class="text-white">example.com</p>
+<div class="mt-22 w-full bg-[#2f6a80] p-4">
+	<a
+		class="mt-2 flex flex-col items-center gap-4"
+		href="https://www.instagram.com/rivnist_pidtrymka/"
+		target="_blank"
+	>
+		<h4 class="border-b-1 text-lg font-bold text-white hover:text-[#1e424f]">
+			Ініціатива ГО "Рівність та підтримка"
+		</h4>
+		<img class="w-24" src="/ni-logo.png" />
+	</a>
 </div>
